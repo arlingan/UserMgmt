9c91444cd3a405eb9749e0d0577fec58f0c0f9b4
Lingannagari <arlingan@CFSAD.COM>
2016-04-28 18:04:16 -0400

    adding client

---
+++ client/js/controllers/LoginCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by arlingan on 3/24/2016.
+ */
+angular.module('LoginCtrl', []).controller('LoginController', function($scope) {
+    
+    $scope.tagline = 'To the moon and back!';
+});
---
+++ client/js/controllers/RegistrationCtrl.js
@@ -0,0 +1,9 @@
+/**
+ * Created by arlingan on 3/24/2016.
+ */
+
+angular.module('RegistrationCtrl', []).controller('RegistrationController', function($scope) {
+
+    $scope.tagline = 'Nothing beats a pocket protector!';
+
+});
