e56272e15d7305a2f6bccdc2d82c1bfce7f5a01b
Lingannagari <arlingan@CFSAD.COM>
2016-04-28 17:41:24 -0400

    Removing directory

--- UserManagement/client/js/controllers/LoginCtrl.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by arlingan on 3/24/2016.
- */
-angular.module('LoginCtrl', []).controller('LoginController', function($scope) {
-    
-    $scope.tagline = 'To the moon and back!';
-});
--- UserManagement/server/db.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by arlingan on 3/24/2016.
- */
-module.exports = {
-    url: 'mongodb://admin:admin@ds064748.mlab.com:64748/usermanagement'
-}
